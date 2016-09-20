c655f95d6ea96ebfb5fcb12f92594eecf9296808
jeromepl <jerome-pl2@hotmail.ca>
2016-09-20 13:52:57 -0400

    Initial commit

---
+++ desktop/src/com/constellationgames/prizm/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.constellationgames.prizm.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.constellationgames.prizm.Prizm;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Prizm(), config);
+	}
+}
