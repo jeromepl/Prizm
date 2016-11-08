943379b0fedad32e9e4d2a634e1bccbf0a529cb7
jeromepl <jerome-pl2@hotmail.ca>
2016-11-08 12:51:53 -0500

    New: Added level selection

--- core/src/com/constellationgames/prizm/screens/LevelSelectScreen.java
+++ core/src/com/constellationgames/prizm/screens/LevelSelectScreen.java
@@ -6,44 +6,87 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.constellationgames.prizm.utils.CustomTextButton;
 
 public class LevelSelectScreen implements Screen {
 	
 	private static final int STANDARD_WIDTH = 576; // Used to scale everything
+	private static final int STANDARD_HEIGHT = 924;
 	private static final float STANDARD_BUTTON_SCALE = 0.13f;
+	private static final int STANDARD_BUTTON_WIDTH = 100;
+	private static final int STANDARD_BUTTON_HEIGHT = 70;
+	private static final int STANDARD_BUTTON_MARGIN = 10;
+	private static final float STANDARD_TITLE_SCALE = 0.4f;
 	
-	private Game game;
-	private Skin skin;
-	private Stage stage = new Stage();
+	private Stage stage = new Stage(new ScreenViewport());
 	
-	private TextButton button;
+	private TextButton[] levelButtons;
+	private TextButton backButton;
+	private Label screenTitle;
 	
-	public LevelSelectScreen(Game game, Skin skin) {
-		this.game = game;
-		this.skin = skin;
+	public LevelSelectScreen(final Game game, final Skin skin) {
+		screenTitle = new Label("Select Level", skin);
 		
-		button = new TextButton("Button", skin);
+		backButton = new CustomTextButton("Back", skin);
+		backButton.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				game.setScreen(new MenuScreen(game));
+			}
+		});
+		
+		// TODO Find all level files
+		levelButtons = new TextButton[12];
+		for (int i = 0; i < levelButtons.length; i++) {
+			final int levelNumber = i + 1;
+			levelButtons[i] = new CustomTextButton("Level " + levelNumber, skin);
+			levelButtons[i].addListener(new ClickListener() {
+				@Override
+				public void clicked(InputEvent event, float x, float y) {
+					game.setScreen(new GameScreen(game, skin, levelNumber));
+				}
+			});
+		}
 	}
 
 	@Override
 	public void show() {
+		Table table = new Table();
+		
 		float xScale = Gdx.graphics.getWidth() * 1.0f / STANDARD_WIDTH;
+		float yScale = Gdx.graphics.getHeight() * 1.0f / STANDARD_HEIGHT;
 		
-		button.getStyle().font.getData().setScale(STANDARD_BUTTON_SCALE * xScale);
+		int buttonsPerRow = ((int)(Gdx.graphics.getWidth() - 2 * STANDARD_BUTTON_MARGIN * xScale)) / ((int) (xScale * (STANDARD_BUTTON_WIDTH + STANDARD_BUTTON_MARGIN)));
 		
-		stage.addActor(button);
+		backButton.setSize(STANDARD_BUTTON_WIDTH * xScale, STANDARD_BUTTON_HEIGHT * 3f/4 * yScale);
+		backButton.getStyle().font.getData().setScale(STANDARD_BUTTON_SCALE * Math.min(xScale, yScale));
+		
+		screenTitle.setFontScale(STANDARD_TITLE_SCALE * xScale);
+		table.add(screenTitle).colspan(buttonsPerRow).spaceBottom(4 * STANDARD_BUTTON_MARGIN).center();
+		
+		for (int i = 0; i < levelButtons.length; i++) {
+			if (i % buttonsPerRow == 0)
+				table.row();
+			
+			levelButtons[i].setSize(STANDARD_BUTTON_WIDTH * xScale, STANDARD_BUTTON_HEIGHT * yScale);
+			table.add(levelButtons[i]).space(STANDARD_BUTTON_MARGIN).size(levelButtons[i].getWidth(), levelButtons[i].getHeight());
+		}
+		table.row();
+		table.add(backButton).colspan(buttonsPerRow).center().space(4 * STANDARD_BUTTON_MARGIN).size(backButton.getWidth(), backButton.getHeight());
+		
+		table.align(Align.center);
+		table.setFillParent(true);
+		stage.addActor(table);
 		
 		Gdx.input.setInputProcessor(stage);
-		
-		button.addListener(new ClickListener() {
-			@Override
-			public void clicked(InputEvent event, float x, float y) {
-				game.setScreen(new GameScreen(game, skin, 1)); //TODO
-			}
-		});
 		
 	}
 
@@ -58,8 +101,7 @@ public class LevelSelectScreen implements Screen {
 
 	@Override
 	public void resize(int width, int height) {
-		// TODO Auto-generated method stub
-		
+		stage.getViewport().update(width, height, true);
 	}
 
 	@Override
