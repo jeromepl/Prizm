1459ce25d13161647661da9883e58ea8ec0eb0ab
jeromepl <jerome-pl2@hotmail.ca>
2016-12-17 10:23:46 -0500

    New: Color selection now shows color buttons

--- core/src/com/constellationgames/prizm/screens/ColorSelectionPopup.java
+++ core/src/com/constellationgames/prizm/screens/ColorSelectionPopup.java
@@ -1,18 +1,35 @@
 package com.constellationgames.prizm.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Pixmap.Format;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.graphics.g2d.NinePatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.Texture;
 import com.constellationgames.prizm.Triangle;
 import com.constellationgames.prizm.utils.TriangleColor;
 
 public class ColorSelectionPopup extends Table {
 	
-	private TextButton color1Button, color2Button;
+	public static final int WIDTH = 210;
+	public static final int HEIGHT = 140;
+	private static final int BUTTON_SIZE = 80;
+	private static final float FONT_SCALE = 0.16f;
+
+	private ImageButton redButton, yellowButton, blueButton;
+	private ClickListener leftListener, rightListener;
+	
 	private Label header;
 	private Triangle fromTriangle, toTriangle;
 	private TriangleColor color1, color2;
@@ -24,36 +41,66 @@ public class ColorSelectionPopup extends Table {
 		
 		this.gameScreen = gameScreen;
 		
-		float fontScale = 0.2f;
+		float fontScale = FONT_SCALE;
 		header = new Label("Split Color", skin);
 		header.setColor(Color.BLACK);
 		header.setFontScale(fontScale);
 		
-		color1Button = new TextButton("Color 1", skin);
-		color2Button = new TextButton("Color 2", skin);
-		color1Button.pad(10);
-		color2Button.pad(10);
+		// Generate buttons of all possible colors
+		redButton = createColorButton(TriangleColor.RED.getColor());
+		blueButton = createColorButton(TriangleColor.BLUE.getColor());
+		yellowButton = createColorButton(TriangleColor.YELLOW.getColor());
 		
-		color1Button.addListener(new ClickListener() {
+		// Initialize the click listeners
+		leftListener = new ClickListener() {
 			@Override
 			public void clicked(InputEvent event, float x, float y) {
 				splitColor(true);
 			}
-		});
-		
-		color2Button.addListener(new ClickListener() {
+		};
+		rightListener = new ClickListener() {
 			@Override
 			public void clicked(InputEvent event, float x, float y) {
 				splitColor(false);
 			}
-		});
+		};
+
+		// Set the size of the popup
+		setWidth(WIDTH);
+		setHeight(HEIGHT);
 		
+		// Add a background with a border
+		NinePatch patch = new NinePatch(new Texture(Gdx.files.internal("images/colorSelectionBackground.png")), 1, 1, 1, 1);
+		NinePatchDrawable background = new NinePatchDrawable(patch);
+		setBackground(background);
+		
+		// Hide the table until necessary
+		setVisible(false);
+	}
+	
+	private ImageButton createColorButton(Color color) {
+		Pixmap pm = new Pixmap(BUTTON_SIZE, BUTTON_SIZE, Format.RGB565);
+		pm.setColor(color);
+		pm.fill();
+		Drawable background = new TextureRegionDrawable(new TextureRegion(new Texture(pm)));
+		
+		ImageButton button = new ImageButton(background);
+		button.setSize(BUTTON_SIZE, BUTTON_SIZE);
+		
+		return button;
+	}
+	
+	private void generateTable(Button b1, Button b2) {
+		clear();
 		add(header).colspan(2).pad(0, 0, 10, 0).center();
 		row();
-		add(color1Button).pad(0, 0, 0, 5);
-		add(color2Button).pad(0, 5, 0, 0);
+		add(b1).pad(0, 0, 0, 5);
+		add(b2).pad(0, 5, 0, 0);
 		
-		setVisible(false);
+		b1.clearListeners();
+		b2.clearListeners();
+		b1.addListener(leftListener);
+		b2.addListener(rightListener);
 	}
 	
 	private void splitColor(boolean pickedColor1) {
@@ -73,14 +120,17 @@ public class ColorSelectionPopup extends Table {
 		case GREEN:
 			color1 = TriangleColor.YELLOW;
 			color2 = TriangleColor.BLUE;
+			generateTable(yellowButton, blueButton);
 			break;
 		case ORANGE:
 			color1 = TriangleColor.YELLOW;
 			color2 = TriangleColor.RED;
+			generateTable(yellowButton, redButton);
 			break;
 		case PURPLE:
 			color1 = TriangleColor.RED;
 			color2 = TriangleColor.BLUE;
+			generateTable(redButton, blueButton);
 			break;
 		default:
 			throw new RuntimeException("Color " + unsplitColor.name() + " is not a secondary color, it cannot be split");
@@ -91,9 +141,6 @@ public class ColorSelectionPopup extends Table {
 			splitColor(toTriangle.getColor() == color2);
 			return;
 		}
-		
-		color1Button.setText(color1.name());
-		color2Button.setText(color2.name());
 		
 		setPosition(x, y);
 		setVisible(true);
